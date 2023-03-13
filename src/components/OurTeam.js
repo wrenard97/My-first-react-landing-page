8cf9614dc59ed32edef106f709095b490092c0c8
Wrenard Guevara <guevarawrenard@gmail.com>
2023-03-12 22:48:59 -0700

    Removed Node Modules

--- src/components/OurTeam.js
+++ src/components/OurTeam.js
@@ -1,45 +1,48 @@
-import React from 'react'
-import teamOne from '../images/team-img1.png'
-import teamTwo from '../images/team-img2.png'
-import teamThree from '../images/team-img3.png'
-
+import React from "react";
+import teamOne from "../images/team-img1.png";
+import teamTwo from "../images/team-img2.png";
+import teamThree from "../images/team-img3.png";
 
 function OurTeam() {
   return (
+    <div
+      className="bg-gray-200 pt-24 pb-28 px-12 text-center font-poppins text-gray-600 text-sm sm:px-44"
+      id="our-team"
+    >
+      <div>
+        <p className="font-crimson-text text-lg">PROFESSIONAL</p>
+        <p className="font-crimson-text text-4xl text-yellow-500 pb-4">
+          OUR TEAM
+        </p>
+      </div>
 
-        <div className='bg-gray-200 pt-24 pb-28 px-12 text-center font-poppins text-gray-600 text-sm sm:px-44' id='our-team'>
-            <div>
-                <p className='font-crimson-text text-lg'>PROFESSIONAL</p>
-                <p className='font-crimson-text text-4xl text-yellow-500 pb-12'>OUR TEAM</p>
-            </div>
-
-            <div className='sm:flex sm:flex-row sm:justify-items-center sm:gap-12'>
-                <div className='pb-6 hover:shadow-2xl sm:pb-0'>
-                    <img src={teamOne} alt="" className='sm:h-96 sm:w-80'/>
-                    <div className='border-x-2 border-b-2 border-gray-300'>
-                        <p className='text-lg font-bold text-black pt-6'>PAUL NARCH</p>
-                        <p className='pb-6'>BARBER</p>
-                    </div>
-                </div>
-
-                <div className='pb-6 hover:shadow-2xl sm:pb-0'>
-                    <img src={teamTwo}alt="" className='sm:h-96 sm:w-80'/>
-                    <div className='border-x-2 border-b-2 border-gray-300'>
-                        <p className='text-lg font-bold text-black pt-6'>STEVEN JOSEPHIN</p>
-                        <p className='pb-6'>BARBER</p>
-                    </div>
-                </div>
-
-                <div className='pb-6 hover:shadow-2xl sm:pb-0'>
-                    <img src={teamThree} alt="" className='sm:h-96 sm:w-80'/>
-                    <div className='border-x-2 border-b-2 border-gray-300'>
-                        <p className='text-lg font-bold text-black pt-6'>ALEXANDER DAVID</p>
-                        <p className='pb-6'>SHAVER</p>
-                    </div>
-                </div> 
-            </div>
+      <div className="sm:flex sm:flex-row sm:justify-items-center sm:gap-12">
+        <div className="pb-6 hover:shadow-2xl sm:pb-0">
+          <img src={teamOne} alt="" className="sm:h-96 sm:w-80" />
+          <div className="border-x-2 border-b-2 border-gray-300">
+            <p className="text-lg font-bold text-black pt-6">PAUL NARCH</p>
+            <p className="pb-6">BARBER</p>
+          </div>
         </div>
-  )
+
+        <div className="pb-6 hover:shadow-2xl sm:pb-0">
+          <img src={teamTwo} alt="" className="sm:h-96 sm:w-80" />
+          <div className="border-x-2 border-b-2 border-gray-300">
+            <p className="text-lg font-bold text-black pt-6">STEVEN JOSEPHIN</p>
+            <p className="pb-6">BARBER</p>
+          </div>
+        </div>
+
+        <div className="pb-6 hover:shadow-2xl sm:pb-0">
+          <img src={teamThree} alt="" className="sm:h-96 sm:w-80" />
+          <div className="border-x-2 border-b-2 border-gray-300">
+            <p className="text-lg font-bold text-black pt-6">ALEXANDER DAVID</p>
+            <p className="pb-6">SHAVER</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default OurTeam
+export default OurTeam;
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,43 +1,61 @@
- 
-import React from 'react'
-import serviceOne from '../images/service-1.png'
-import serviceTwo from '../images/service-2.png'
-import serviceThree from '../images/service-3.png'
-
+import React from "react";
+import serviceOne from "../images/service-1.png";
+import serviceTwo from "../images/service-2.png";
+import serviceThree from "../images/service-3.png";
 
 function Services() {
-    const buttonStyle= "bg-black rounded-full w-32 h-9 text-white hover:bg-yellow-500 hover:border-yellow-500"
+  const buttonStyle =
+    "bg-black rounded-full w-32 h-9 text-white hover:bg-yellow-500 hover:border-yellow-500";
   return (
+    <div
+      className="bg-gray-200 pt-24 pb-28 px-12 text-center font-poppins text-gray-600 text-xs sm:px-48"
+      id="services"
+    >
+      <div>
+        <p className="font-crimson-text text-lg">NOTHING BUT THE BEST</p>
+        <p className="font-crimson-text text-4xl text-yellow-500 pb-4">
+          OUR SERVICES
+        </p>
+      </div>
 
-    <div className='bg-gray-200 pt-24 pb-28 px-12 text-center font-poppins text-gray-600 text-xs sm:px-48' id='services'>  
-        <div>
-            <p className="font-crimson-text text-lg">NOTHING BUT THE BEST</p>
-            <p className="font-crimson-text text-4xl text-yellow-500">OUR SERVICES</p>
+      <div className="flex flex-col justify-items-center gap-8 sm:flex sm:flex-row sm:gap-12">
+        <div className="pb-6 hover:shadow-2xl">
+          <img src={serviceOne} alt="" className="sm:h-52 sm:w-96" />
+          <h3 className="text-lg font-bold text-black pt-6 pb-3">
+            CLASSIC HAIRCUT
+          </h3>
+          <p className="pb-4 px-8 tracking-wider sm:px-6">
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+          <button className={buttonStyle}>READ MORE</button>
         </div>
-
-        <div className="flex flex-col justify-items-center sm:flex sm:flex-row sm:gap-12">
-            <div className='py-6 hover:shadow-2xl'>
-                <img src={serviceOne} alt="" className='sm:h-52 sm:w-96'/>
-                <h3 className="text-lg font-bold text-black pt-6 pb-3">CLASSIC HAIRCUT</h3>
-                <p className="pb-4 px-8 tracking-wider sm:px-6"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </p>
-                <button className={buttonStyle}>READ MORE</button>
-            </div>
-            <div className='py-6 hover:shadow-2xl'>
-                <img src={serviceTwo} alt="" className='sm:h-52 sm:w-96'/>
-                <h3 className="text-lg font-bold text-black pt-6 pb-3">CLIPPER CUT</h3>
-                <p className="pb-4 px-8 tracking-wider sm:px-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                <button className={buttonStyle}>READ MORE</button>
-            </div>
-            <div className='py-6 hover:shadow-2xl'>
-                <img src={serviceThree} alt="" className='sm:h-52 sm:w-96'/>
-                <h3 className="text-lg font-bold text-black pt-6 pb-3">RAZOR SHAVER</h3>
-                <p className="pb-4 px-8 tracking-wider sm:px-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                <button className={buttonStyle}>READ MORE</button>
-            </div>
+        <div className="pb-6 hover:shadow-2xl">
+          <img src={serviceTwo} alt="" className="sm:h-52 sm:w-96" />
+          <h3 className="text-lg font-bold text-black pt-6 pb-3">
+            CLIPPER CUT
+          </h3>
+          <p className="pb-4 px-8 tracking-wider sm:px-6">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+          <button className={buttonStyle}>READ MORE</button>
         </div>
+        <div className="pb-6 hover:shadow-2xl">
+          <img src={serviceThree} alt="" className="sm:h-52 sm:w-96" />
+          <h3 className="text-lg font-bold text-black pt-6 pb-3">
+            RAZOR SHAVER
+          </h3>
+          <p className="pb-4 px-8 tracking-wider sm:px-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+          <button className={buttonStyle}>READ MORE</button>
+        </div>
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default Services
+export default Services;
